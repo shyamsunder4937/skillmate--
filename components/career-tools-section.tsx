bfae196b1dbe0edfc545a38e48af3d5834aa4d2a
shyam <shyamsunder4937@gmail.com>
2025-11-10 19:05:19 +0530

    Update career tools section

--- components/career-tools-section.tsx
+++ components/career-tools-section.tsx
@@ -47,7 +47,7 @@ const tools = [
     icon: Search,
     color: "text-red-600",
     bgColor: "bg-red-50",
-    url: "https://7kk5xcvx-3000.inc1.devtunnels.ms/",
+    url: "https://nhhbqhb4-3000.inc1.devtunnels.ms/login",
     gradient: "from-red-500 to-red-700"
   },
  
