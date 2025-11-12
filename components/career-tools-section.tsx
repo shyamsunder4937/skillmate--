0371de04cb72aba701ad1e1c677c9163e528ce1a
shyam <shyamsunder4937@gmail.com>
2025-11-12 12:25:30 +0530

    Updan

--- components/career-tools-section.tsx
+++ components/career-tools-section.tsx
@@ -47,7 +47,7 @@ const tools = [
     icon: Search,
     color: "text-red-600",
     bgColor: "bg-red-50",
-    url: "https://nhhbqhb4-3001.inc1.devtunnels.ms/auth/postback/github?tunnel=1",
+    url: "https://nhhbqhb4-3000.inc1.devtunnels.ms/",
     gradient: "from-red-500 to-red-700"
   },
  
